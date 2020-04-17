1d840c438f122ce7c59db7d85cde30d7b5d1b983
Oleg Koryakov <outflowpwnz@gmail.com>
2020-04-17 15:01:47 +0700

    Добавил файл с функицями помощниками

---
+++ src/ts/slider/helpers.ts
@@ -0,0 +1,31 @@
+function getModifier(element: JQuery): string | undefined {
+  const classList = element.attr('class');
+  if (classList !== undefined) {
+    return classList.split('--')[1];
+  }
+  return undefined;
+}
+
+function getSliderHTML() {
+  return $(`<div class="slider slider--horizontal">
+              <div class="slider__values">
+                <input type="text" class="slider__input-value slider__input-value--from">
+                <input type="text" class="slider__input-value slider__input-value--to">
+              </div>
+              <div class="slider__line">
+              <div class="slider__thumb slider__thumb--from">
+                <div class="slider__value"></div>
+              </div>
+              <div class="slider__thumb slider__thumb--to">
+                <div class="slider__value"></div>
+              </div>
+              <div class="slider__range-line"></div>
+              </div>
+            </div>`);
+}
+
+// function isValidInputValue(value: string | number) {
+//   return value !== undefined || value !== '';
+// }
+
+export { getModifier, getSliderHTML };
