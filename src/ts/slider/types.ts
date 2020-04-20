0dcba2c0bc9d1e53bb0263869712ec6e16f366c6
Oleg Koryakov <outflowpwnz@gmail.com>
2020-04-20 12:00:18 +0700

    обновил старые, добавил новые интерфейсы и типы

--- src/ts/slider/types.ts
+++ src/ts/slider/types.ts
@@ -17,9 +17,52 @@ interface IEventEmitter {
 interface IModel{
   state: IState,
   step: number,
+  getRangeValues(): IRangeValues,
+  getValues(): TValues,
+  getState(): IState,
+  setValues(values: TValues): void,
   toggleOrientation(): void,
   toggleRange(): void,
   toggleShowValue(): void,
   setStep(step: number): void,
   getStep(): number,
+}
+
+interface INumberValues {
+  from: number,
+  to: number,
+}
+
+type TStringValues = string[];
+
+type TValues = INumberValues | TStringValues;
+
+interface IRangeValues {
+  min: number,
+  max: number,
+}
+
+interface ISliderOptions {
+  position: 'left' | 'top',
+  clientAxis: 'clientX' | 'clientY';
+}
+
+interface IView extends IEventEmitter {
+  slider: JQuery,
+  setSliderInitialState(state: IState): void,
+  toggleOrientation(): void,
+  toggleRange(): void,
+  toggleShowValue(): void,
+  getSliderWidth(): number,
+  getSliderOptions(): ISliderOptions,
+  setThumbValue(thumbElement: JQuery, value: number | string): void,
+  setSliderRangeLineCoords(): void,
+  addDragNDropHandlers(): void,
+  setInputValue(value: string | number, modifier: string): void,
+}
+
+interface IThumbOptions {
+  value?: number | string,
+  coord?: number
+  thumb: JQuery
 }
