4b0cd767ce2a9ab06793cfc7960cc3b7d551ff83
Oleg Koryakov <outflowpwnz@gmail.com>
2020-04-28 11:27:58 +0700

    настроил jest для тестов с использованием jquery

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,12 @@
 module.exports = {
-  roots: ['<rootDir>/src'],
+  transformIgnorePatterns: [
+    '.\node_modules\\@types\\jquery',
+    '.\node_modules\\jquery',
+  ],
+  setupFiles: ['./jest-setup.js'],
   transform: {
     '^.+\\.ts$': 'ts-jest',
   },
   testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.ts$',
   moduleFileExtensions: ['ts', 'js'],
-}
+};
