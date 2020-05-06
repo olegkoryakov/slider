67cfb1f1da5e3c856f8bdf0cb8d7b46326bb0e4b
Oleg Koryakov <outflowpwnz@gmail.com>
2020-05-06 11:11:48 +0700

    обновил интерфейсы и типы для новой структуры приложения

--- src/ts/slider/types.ts
+++ src/ts/slider/types.ts
@@ -15,15 +15,15 @@ interface IEventEmitter {
 }
 
 interface IModel{
-  state: IState,
-  step: number,
+  _state: IState,
+  _step: number,
   getRangeValues(): IRangeValues,
   getValues(): TValues,
   getState(): IState,
   setValues(values: TValues): void,
-  toggleOrientation(): void,
-  toggleRange(): void,
-  toggleShowValue(): void,
+  setOrientationState(orientation: IState['orientation']): void,
+  setRangeState(rangeState: IState['isRange']): void,
+  setShowValueState(showValue: IState['isShowValue']): void,
   setStep(step: number): void,
   getStep(): number,
 }
@@ -47,23 +47,107 @@ interface ISliderOptions {
   clientAxis: 'clientX' | 'clientY';
 }
 
-interface IView extends IEventEmitter {
-  slider: JQuery,
-  configPanel: JQuery,
-  setSliderInitialState(state: IState): void,
-  toggleOrientation(): void,
-  toggleRange(): void,
-  toggleShowValue(): void,
-  getSliderWidth(): number,
-  getSliderOptions(): ISliderOptions,
-  setThumbValue(thumbElement: JQuery, value: number | string): void,
-  setSliderRangeLineCoords(): void,
-  addDragNDropHandlers(): void,
-  setInputValue(value: string | number, modifier: string): void,
+type TModifier = 'from' | 'to';
+
+interface IThumbView {
+  _thumb: JQuery,
+  _modifier: TModifier,
+  _node: JQuery,
+  getModifier(): TModifier,
+  appendToNode(): void,
+  removeFromDOM(): void,
+  isInDOM(): boolean,
+  showValue(): void,
+  hideValue(): void,
+  isValueShowing(): boolean,
+  setValue(value: string | number): void,
+  setPosition(position: string, coord: number): void,
+  getCoord(position: ISliderOptions['position']): number;
+  getWidth(): number,
+  addDragNDropHandler(
+    getOptionsCallback: Function,
+    emitCallback: Function,
+    emitCallbackType: string,
+    getWidthCallback: Function,
+  ): void,
 }
 
-interface IThumbOptions {
-  value?: number | string,
-  coord?: number
-  thumb: JQuery
+interface IRangeLineView {
+  _rangeLine: JQuery;
+  _node: JQuery;
+  setRangeLineSizeFromCoords(
+    coordFrom: number,
+    coordTo: number,
+    gap: number,
+    position: ISliderOptions['position'],
+  ): void;
+  setOrientation(
+    oldPos: ISliderOptions['position'],
+    newPos: ISliderOptions['position']
+  ): void,
+  appendToNode(): void;
+  isInDOM(): boolean;
+  removeFromDOM(): void;
+}
+
+interface ISliderView extends IEventEmitter{
+  _inputValueFrom: IValueInputView,
+  _inputValueTo: IValueInputView,
+  _thumbFrom: IThumbView,
+  _thumbTo: IThumbView,
+  _rangeLine: IRangeLineView,
+  _slider: JQuery,
+  _setInitialState(state: IState, node: JQuery): void,
+  _onSliderLineClick(clickE: JQuery.ClickEvent): void,
+  setOrientation(orientation: IState['orientation']): void,
+  getThumbByModifier(modifier: TModifier): IThumbView;
+  getInputByModifier(modifier: TModifier): IValueInputView;
+  setRange(isRange: IState['isRange']): void,
+  setShowValue(isShowValue: IState['isShowValue']): void,
+  calcValues(): void;
+  getOptions(): ISliderOptions,
+  isRange(): boolean,
+  resizeRangeLine(): void,
+  getWidth(): number,
+}
+
+interface IConverter {
+  coordInValue(
+    coord: number,
+    step: number,
+    width: number,
+    range: IRangeValues,
+  ): number,
+  valueInCoord(
+    valueOrIndex: number,
+    width: number,
+    range: IRangeValues,
+  ): number,
+  calcValueWithStep(value: number, step: number): number,
+}
+
+interface IValueInputView {
+  _valueInput: JQuery,
+  _modifier: TModifier,
+  _node: JQuery;
+  appendToNode(): void,
+  removeFromDOM(): void,
+  setValue(value: number | string): void,
+  getValue(): string,
+  getModifier(): TModifier,
+  addChangeHandler(
+    emitCallback: Function,
+    emitCallbackType: string,
+  ): void,
+}
+
+interface IRangeInstances {
+  thumb: IThumbView,
+  input: IValueInputView,
+}
+
+interface IConfigPanelView extends IEventEmitter {
+  _configPanel: JQuery,
+  _callbackPrefix: string,
+  addConfigPanelHandlers(emitCallback: Function): void,
 }
